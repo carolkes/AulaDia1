e76c06fbb548118a200527e781272db3d16a580e
carolkes <carolkestering@hotmail.com>
2019-09-11 14:44:32 -0300

    Commit-tamanho-nome

---
+++ Console Numeros Primos/Console Numeros Primos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificarTamanhoDoNome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informar o nome a ser calculado:");
+            var nomeLenght = Console.ReadLine();
+
+            Console.WriteLine($"O número de caracteres deste nome é: {nomeLenght.Replace(" ","").Length}");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
