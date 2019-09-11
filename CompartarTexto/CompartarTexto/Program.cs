bed393a230f09171ed32a53509f043900618f374
carolkes <carolkestering@hotmail.com>
2019-09-11 15:58:32 -0300

    commit-matriz

---
+++ CompartarTexto/CompartarTexto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompartarTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeiroTexto = "Era uma vez Caroline que gostava muito de cantar e amassar seus cachorros. Os cachorros da Caroline se chamavam madu, ollie e peca. Caroline cantava pros pássaros, enquanto lavava a louça e enquanto abraçava seus cachorros. Caroline era feliz.";
+
+            primeiroTexto = primeiroTexto.Replace("Caroline", "C").Replace("cachorros", "T");
+
+
+
+            Console.WriteLine(primeiroTexto);
+            Console.WriteLine($"Texto contém: {primeiroTexto.Length} caracteres.");
+
+         
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ MatrizArray/MatrizArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayLinear = new string[3];
+
+            arrayLinear[0] = "Caroline";
+            arrayLinear[1] = "Marcus";
+            arrayLinear[2] = "Madu";
+
+            List<string> listaLinear = new List<string>
+            {
+                "Caroline",
+                "Marcus",
+                "Madu",
+                "Ollie"
+            };
+
+            Console.WriteLine(arrayLinear[0]);
+            Console.WriteLine(listaLinear[0]);
+            var keypress =  Console.ReadKey().KeyChar.ToString();
+
+        }
+    }
+}
