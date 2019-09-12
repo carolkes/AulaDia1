c521702c01944fd1dbe864f013ca5781203d9250
carolkes <carolkestering@hotmail.com>
2019-09-12 10:27:46 -0300

    commit

---
+++ LacosDeRepeticao/UtilizandoFor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nome = "Meu nome é Caroline Kestering Freire porque sou casada com o Vinícius Freire";
+
+            var countFind = 0;
+
+            Console.WriteLine("Encontre a palavra de até 3 caracteres:");
+            var palavra = Console.ReadLine();
+
+            for (int i = 0; i < (nome.Length -2); i++)
+            {
+                var palavraParaComparar = nome[i].ToString() +
+                                          nome[i + 1].ToString() +
+                                          nome[i + 2].ToString();
+
+                if (palavra == palavraParaComparar)
+                    countFind++;
+
+            }
+
+            Console.WriteLine($"Quantidade total de '{palavra}' {countFind}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LacosDeRepeticao/UtilizandoWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+
+            while (count < 10)
+            {
+                Console.WriteLine($"Número:{count}");
+                count++;
+            }
+
+            Console.WriteLine("Terminou o loop");
+            Console.ReadKey();
+
+        }
+    }
+}
