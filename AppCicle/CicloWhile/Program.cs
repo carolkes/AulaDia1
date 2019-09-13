46fb8e188fc045e8e194c94a7ee24737486bc65c
carolkes <carolkestering@hotmail.com>
2019-09-13 14:00:42 -0300

    commit

---
+++ AppCicle/CicloWhile/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicloWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowInitAppText();
+
+            //informaçao convertida para string / KeyChar é como se fosse a letra de uma palavra
+            //para passar uma string para int, utilizar o 'Parse'
+            while (AskToContinue() == 1) 
+            //"1" para converter pra string ou alterar a var para int, acrescentando int.Parse
+            {
+                
+            }
+        }
+        /// <summary>
+        /// Metodo para mostrar o texto inicial do sistema alone
+        /// </summary>
+        private static  void    ShowInitAppText()
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("----Seja Bem Vindo!----");
+            Console.WriteLine("-----------------------");
+
+        }
+        /// <summary>
+        /// Método para realizar a pergunta que se deseja continuar
+        /// </summary>
+        /// <returns>retorna o valor que foi inserido</returns>
+        private static  int AskToContinue()
+        {
+            Console.WriteLine("Vamos conversar? Sim(1) Não(2)");
+            return int.Parse(Console.ReadKey().KeyChar.ToString());
+
+        }
+    }
+}
---
+++ AppCicle/ExercicioWhileIdade/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioWhileIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowInitAppText();
+
+            string nome = string.Empty;
+            int idade = 0;
+
+
+            while (AskToContinue() == 1)
+            {
+                nome = AskToName();
+                idade = AskToAge(nome);
+
+            }
+          
+            Console.WriteLine($"Suas informações: Nome {nome} e sua idade {idade}");
+            Console.ReadKey();
+        }
+
+        private static void ShowInitAppText()
+        {
+            Console.WriteLine("************************");
+            Console.WriteLine("**Olá! Seja Bem Vindo!**");
+            Console.WriteLine("************************");
+        }
+
+        private static int AskToContinue()
+        {
+            Console.WriteLine("Vamos conversar? Sim(1) Não(2)");
+            return int.Parse(Console.ReadKey().KeyChar.ToString());
+
+        }
+
+        private static int AskToAge(string  nome)
+        {
+            Console.WriteLine("Informe a sua idade:");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18)
+                Console.WriteLine($"{nome} você pode beber!!");
+            else
+            
+                Console.WriteLine($"{nome} você ainda não pode beber..");
+
+            return idade;
+        }
+        private static string AskToName()
+        {
+            Console.WriteLine("Qual seu nome?");
+            return Console.ReadLine();
+        }
+
+        
+        
+
+      
+       
+     
+
+
+
+        
+
+
+
+
+
+
+
+
+
+        
+        
+       
+        
+
+
+
+
+    }
+}   
+	
+
+	
