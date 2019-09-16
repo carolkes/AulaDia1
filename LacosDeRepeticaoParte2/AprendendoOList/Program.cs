5a665b08b718fcbcf0de3501100617ca7395a5b2
carolkes <carolkestering@hotmail.com>
2019-09-16 17:22:08 -0300

    commit

---
+++ LacosDeRepeticaoParte2/AprendendoOList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoOList
+{
+    class Program
+    {
+         static List<string> minhaListaPulgmatica = new List<string>()
+            {
+                "Felipe",
+                "Irineu",
+                "Serilop",
+            };
+
+        static void Main(string[] args)
+        {
+  
+            AdicionarItensALista();
+
+            ListaInformacoes();
+
+            Console.ReadKey();
+        }
+
+        private static  void    AdicionarItensALista()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe um nome:");
+            minhaListaPulgmatica.Add(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Deseja Informar mais valores? sim(S) não(N)");
+
+            if (Console.ReadKey().KeyChar.ToString().ToLower() == "s")
+            AdicionarItensALista();
+        }
+        /// <summary>
+        /// Método que mostra  a lista
+        /// </summary>
+        private static  void    ListaInformacoes()
+        {
+            Console.Clear();
+            Console.WriteLine("Nomes adicionados a lista");
+            foreach (var item in minhaListaPulgmatica)
+                Console.WriteLine($"Nome:{item} foi adicionado a lista");
+        }
+        
+    }
+}
