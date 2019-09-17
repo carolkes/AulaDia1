fb71398d4dd9d6660e8ef70705232c51554d5408
carolkes <carolkestering@hotmail.com>
2019-09-17 17:23:26 -0300

    CommitExerc

---
+++ Solution2/ExerciciosCrazy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCrazy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DesenhoArvore();
+        }
+
+        private static  void    DesenhoArvore()
+        {
+            Console.WriteLine("            $$$$$$$$$$             ");
+            Console.WriteLine("          $$$$$$$$$$$$$$           ");
+            Console.WriteLine("         $$$$$$$$$$$$$$$$$                     ");
+            Console.WriteLine("        $$$$$$$$$$$$$$$$$$$                      ");
+            Console.WriteLine("       $$$$$$$$$$$$$$$$$$$$$               ");
+            Console.WriteLine("      $$$$$$$$$$$$$$$$$$$$$$$                              ");
+            Console.WriteLine("       $$$$$$$$$$$$$$$$$$$$$                ");
+            Console.WriteLine("        $$$$$$$$$$$$$$$$$$$               ");
+            Console.WriteLine("         $$$$$$$$$$$$$$$$$              ");
+            Console.WriteLine("          $$$$$$$$$$$$$$             ");
+            Console.WriteLine("             ||||||||          ");
+            Console.WriteLine("             ||||||||          ");
+            Console.WriteLine("             ||||||||         ");
+            Console.WriteLine("             ||||||||        ");
+            Console.WriteLine("             ||||||||        ");
+            Console.WriteLine("             ||||||||     *  ");
+            Console.WriteLine("             ||||||||     |   ");
+            Console.WriteLine("      MMMMMMMMMMMMMMMMMMMMMMMMM   ");
+
+            Console.ReadKey();
+        }
+    }
+}
