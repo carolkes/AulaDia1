badfd94959ed319b7f42972ad78e5ccc8e744b63
carolkes <carolkestering@hotmail.com>
2019-09-17 08:21:28 -0300

    commit

---
+++ Atividade/Atividade05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira aqui uma palavra");
+            var palavra = Console.ReadLine();
+
+
+        }
+    }
+}
