bec28080d8ad20dd821e924b99133c3a96dba716
carolkes <carolkestering@hotmail.com>
2019-09-27 15:18:28 -0300

    commitComComentários

--- SistemaDeCervejas/ListagemDeCervejas/Controller/CervejaController.cs
+++ SistemaDeCervejas/ListagemDeCervejas/Controller/CervejaController.cs
@@ -10,18 +10,34 @@ namespace ListagemDeCervejas.Controller
     public class CervejaController
     {
         SistemaCervejaContext cervejaContext = new SistemaCervejaContext();
+        /// <summary>
+        /// Método que retorna o valor total de litros das cervejas listadas
+        /// </summary>
+        /// <returns></returns>
         public double ValorTotalEmLitros()
         {
             return cervejaContext.ListaCervejasPublica.Sum(x => x.Litros);
         }
+        /// <summary>
+        /// Método que retorna o valor total das cervejas listadas
+        /// </summary>
+        /// <returns>Retornamos o valor total em double</returns>
         public double ValorTotalCervejas()
         {
            return cervejaContext.ListaCervejasPublica.Sum(x => x.Valor);
         }
+        /// <summary>
+        /// Método que retorna a lista de cervejas
+        /// </summary>
+        /// <returns>Retorna a lista de cervejas</returns>
         public List<Cerveja> GetCervejas()
         {
                 return cervejaContext.ListaCervejasPublica;  
         }
+        /// <summary>
+        /// Método que pemite o usuario adicionar mais cervejas
+        /// </summary>
+        /// <param name="parametroCerveja"></param>
         public void AdicionarCerveja(Cerveja parametroCerveja)
         {
             parametroCerveja.Id = cervejaContext.IdContadorCervejas++;
