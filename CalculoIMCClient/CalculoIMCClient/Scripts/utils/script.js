babe06abce02445d79ec9c80034c9504dc2c7d46
carolkes <carolkestering@hotmail.com>
2019-10-28 13:34:15 -0300

    preciso terminar

--- CalculoIMCClient/CalculoIMCClient/Scripts/utils/script.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function () {
-
-    var url = `http://localhost:55426/api/CalculoIMC?nomeUsuario={nomeUsuario}&pesoUsuario={pesoUsuario}&alturaUsuario={alturaUsuario}`;
-    $.get(url);
-
-})
-
