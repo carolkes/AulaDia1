a82b9dc87d61bc407672055c5333b181d3809432
carolkes <carolkestering@hotmail.com>
2019-10-28 17:01:27 -0300

    não sei o que colocar no lugar do alert pra aparecer as informações + começamos lista no c# vizualizando com postman

--- CalculoIMC/CalculoIMC/Controllers/CalculoIMCController.cs
+++ CalculoIMC/CalculoIMC/Controllers/CalculoIMCController.cs
@@ -5,17 +5,48 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using CalculoIMC.Models;
 
 namespace CalculoIMC.Controllers
 {
     public class CalculoIMCController : ApiController
     {
+        List<ObjetoIMC> listaDeObjetos = new List<ObjetoIMC>()
+        {
+            new ObjetoIMC(){Nome = "Carol"},
+            new ObjetoIMC(){Nome = "Camila"},
+            new ObjetoIMC(){Nome = "Brigitte"},
+        };
+
+
+        public List<ObjetoIMC> Get()
+        {
+            return listaDeObjetos;
+        }
+
         [EnableCors(origins: "*", headers: "*", methods: "*")]
         public string Get(string nomeUsuario, double pesoUsuario, double alturaUsuario)
         {
            var imc =  pesoUsuario / (alturaUsuario * alturaUsuario);
 
-            return $"Olá {nomeUsuario}, seu IMC é {imc} e ele foi calculado de acordo com sua altura {alturaUsuario} e peso {pesoUsuario} Kg.";
+            if (imc <= 24.9)
+            {
+                return $"Olá {nomeUsuario}, seu IMC é de {imc.ToString("N2")}, o que significa que o seu peso está ideal!";
+            }
+            else
+            {
+                return $"Olá {nomeUsuario}, seu IMC é {imc.ToString("N2")}. Infelizmente você está com sobrepeso!";
+            } 
+        }
+        public ObjetoIMC Post(ObjetoIMC values)
+        {
+            ObjetoIMC imcCalc = values;
+
+            imcCalc.ImcValue = imcCalc.Peso / (imcCalc.Altura * imcCalc.Altura);
+
+            listaDeObjetos.Add(imcCalc);
+
+            return imcCalc;
         }
     }
 }
--- CalculoIMC/CalculoIMCClient/CalculoIMCClient/Scripts/utils/script.js
+++ CalculoIMC/CalculoIMCClient/CalculoIMCClient/Scripts/utils/script.js
@@ -1,26 +1,23 @@
-﻿var teste = `<p>
-    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#collapse${id}" aria-expanded="false" aria-controls="collapseExample">
-        Calcular IMC
-    </button>
-    </p>
-    <div class="collapse" id="collapse_id">
-        <div class="card card-body">
-            ${data}
-        </div>
-    </div>`
+﻿//var teste = `<p>
+//    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#collapse${id}" aria-expanded="false" aria-controls="collapseExample">
+//        Calcular IMC
+//    </button>
+//    </p>
+//    <div class="collapse" id="collapse_id${id}">
+//        <div class="card card-body">
+//            ${data}
+//        </div>
+//    </div>`
 
 $(document).ready(function () {
 
     $('#btncalcular').click(function () {
+
         var nome = $('#form_principal').serialize();
         var url = `http://localhost:55426/api/CalculoIMC?${nome}`;
+
         $.get(url, function (data) {
-
-
-
-            $('#collapse_id').append(
-                teste
-            );  
+            alert(data);
         });
     });
 
