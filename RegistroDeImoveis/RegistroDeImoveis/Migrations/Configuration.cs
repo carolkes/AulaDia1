8edede61a7c809f077a5c60199b6268618c1cbaf
carolkes <carolkestering@hotmail.com>
2019-10-30 17:09:38 -0300

    Exercicio api web Cadastro de Imoveis Terminado

---
+++ RegistroDeImoveis/RegistroDeImoveis/Migrations/Configuration.cs
@@ -0,0 +1,40 @@
+﻿namespace RegistroDeImoveisMigrationApiWeb.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using RegistroDeImoveisMigrationApiWeb.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RegistroDeImoveisMigrationApiWeb.Models.MigrationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(RegistroDeImoveisMigrationApiWeb.Models.MigrationContext context)
+        {
+            context.Proprietarios.AddOrUpdate(x => x.Nome, new Models.Proprietario() {
+                Nome = "Carol",
+                DataNascimento = new DateTime(1995,11,21),
+                Email = "carol@gmail.com"
+            });
+
+            context.SaveChanges();
+
+            var proprietarioId = context.Proprietarios.FirstOrDefault(x => x.Nome == "Carol").Id;
+
+            context.Imoveis.AddOrUpdate(x => x.ProprietarioId, new Models.Imovel() {
+                Cep = "89010401",
+                Logradouro = "Nereu Ramos",
+                Bairro = "Jardim Blumenau",
+                Numero = 515,
+                Municipio = "Blumenau",
+                ProprietarioId = proprietarioId
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ RegistroDeImoveis/RegistroDeImoveis/Models/MigrationContext.cs
@@ -0,0 +1,16 @@
+﻿using RegistroDeImoveisMigrationApiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RegistroDeImoveisMigrationApiWeb.Models
+{
+    public class MigrationContext : DbContext
+    {
+        public DbSet<Proprietario> Proprietarios { get; set; }
+        public DbSet<Imovel> Imoveis { get; set; }
+    }
+
+}
