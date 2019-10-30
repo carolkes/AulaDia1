81bcceffe7a41077d7dde8cbd01118cf22fe3635
carolkes <carolkestering@hotmail.com>
2019-10-30 14:24:34 -0300

    Explicação ConsoleMigration

---
+++ Migrations/ConsoleMigration1/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleMigration1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ConsoleMigration1.Model.MigrationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ConsoleMigration1.Model.MigrationContext";
+        }
+
+        protected override void Seed(ConsoleMigration1.Model.MigrationContext context)
+        {
+            context.Usuarios.AddOrUpdate(x => x.Nome == "Carol", new Model.Usuario()
+            {
+                Nome = "Carol",
+                Login = "admin",
+                Senha = "admin",
+                Email = "admin@admin.com.br",
+                Nivel = Model.Nivel.Administrador
+            });
+
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Migrations/ConsoleMigration1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleMigration1.Model;
+
+namespace ConsoleMigration1
+{
+    class Program
+    {
+        static MigrationContext db = new MigrationContext();
+        static void Main(string[] args)
+        {
+            db.Usuarios.ToList().ForEach(x => Console.WriteLine(x.Nome));
+            Console.ReadKey();
+        }
+    }
+}
