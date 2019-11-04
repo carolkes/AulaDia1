6a10ea4af624d5f628f8e0f4601d3304860badf8
carolkes <carolkestering@hotmail.com>
2019-11-04 15:50:06 -0300

    exercicio 1 04/11 - falta o get pegar todos os post

---
+++ WebApiCorePesoObjeto/WebApiCorePesoObjeto/Controllers/CalcController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiCorePesoObjeto.Model;
+
+namespace WebApiCorePesoObjeto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+
+        List<Objeto> listaDeObjetos = new List<Objeto>();
+
+        [HttpGet]
+        public List<Objeto> Get(Objeto item)
+        {
+            listaDeObjetos.Add(item);
+
+            return listaDeObjetos;
+        }
+
+        [HttpPost]
+        public Objeto Post(Objeto values)
+        {
+            listaDeObjetos.Add(values);
+
+            return values;  
+        }
+    }
+}
---
+++ WebApiCorePesoObjeto/WebApiCorePesoObjeto/Model/Objeto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCorePesoObjeto.Model
+{
+    public class Objeto
+    {
+        [Key]
+        public int Id { get; set; }
+        public double Densidade { get; set; }
+        public double Volume { get; set; }
+        public double Gravidade { get; set; } = 9.81;
+        public double Peso { get { return Densidade * Volume * Gravidade; } }
+
+    }
+}
